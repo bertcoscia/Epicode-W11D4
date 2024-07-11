4c35a849434040c125bd187da064094b589b98c5
A. Alberto Coscia <angeloalberto.coscia@gmail.com>
2024-07-11 15:40:32 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return <div className="text-center my-5">App in React con TS</div>;
+}
+
+export default App;
